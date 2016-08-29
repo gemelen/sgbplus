cf57d55a34a408bfd5bb5c76bc9622e8d1454300
gemelen <gemelen@gmail.com>
2016-08-30 01:06:51 +0300

    Cleanup dependencies, add akka and logging

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,7 @@ object Versions {
   val Scopt               = "3.3.0"
   val Config              = "1.3.0"
   val Akka                = "2.4.9"
+  val Jackson             = "2.7.5"
 }
 
 object Dependencies {
@@ -12,29 +13,22 @@ object Dependencies {
 
   // scalastyle:off
 
-  val jacksonDatabind     = "com.fasterxml.jackson.core" % "jackson-databind" % "2.3.5"
-  val jacksonDataformat   = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-xml" % "2.3.5"
-  val jacksonScala        = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.5.3"
+  val akka                = Seq (
+    "com.typesafe.akka" %% "akka-actor" % Versions.Akka,
+    "com.typesafe.akka" %% "akka-remote" % Versions.Akka,
+    "com.typesafe.akka" %% "akka-cluster" % Versions.Akka,
+    "com.typesafe.akka" %% "akka-http-experimental" % Versions.Akka,
+    "com.typesafe.akka" %% "akka-slf4j" % Versions.Akka
+  )
 
-  // apache
-  val apacheCommonsLang   = "org.apache.commons" % "commons-lang3" % "3.2"
-  val apacheCompress      = "org.apache.commons" % "commons-compress" % "1.9"
-  val apacheHttpClient    = "org.apache.httpcomponents" % "httpclient" % "4.5.1"
-  val apacheIO            = "commons-io" % "commons-io" % "2.4"
-  val apacheCommonsPool   = "org.apache.commons" % "commons-pool2" % "2.3"
-
-  val amazonAws           = "com.amazonaws" % "aws-java-sdk" % "1.6.4" excludeAll(
-    ExclusionRule(organization = "org.javassist", name = "javassist"),
-    ExclusionRule(organization = "org.apache.httpcomponents", name = "httpclient"))
-
-  // misc
-  val quartz              = "org.quartz-scheduler" % "quartz" % "2.2.1"
-  val guava               = "com.google.guava" % "guava" % "18.0"
-  val javaxMail           = "javax.mail" % "mail" % "1.4.7"
-  val userAgentTools      = "eu.bitwalker" % "UserAgentUtils" % "1.19"
+  val jacksonDatabind     = "com.fasterxml.jackson.core" % "jackson-databind" % Versions.Jackson
+  val jacksonDataformat   = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-xml" % Versions.Jackson
+  val jacksonScala        = "com.fasterxml.jackson.module" %% "jackson-module-scala" % Versions.Jackson
 
   // logging
-  val log4j               = "org.slf4j" % "slf4j-api" % "1.7.10"
+  val slf4j               = "org.slf4j" % "slf4j-api" % "1.7.21"
+  val logBack-core        = "ch.qos.logback" % "logback-core" % "1.1.7"
+  val logBack-classic     = "ch.qos.logback" % "logback-classic" % "1.1.7"
 
  // configuration
   val scopt                 = "com.github.scopt"           %% "scopt"                       % Versions.Scopt
