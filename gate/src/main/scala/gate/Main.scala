e79667fa7757232abeb5db14528e107075de719d
gemelen <gemelen@gmail.com>
2016-09-09 00:27:02 +0300

    Rewriting of main

--- gate/src/main/scala/gate/Main.scala
+++ gate/src/main/scala/gate/Main.scala
@@ -8,6 +8,9 @@ import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 class Main {
 
 }
@@ -15,26 +18,31 @@ class Main {
 object Main extends SLF4JLogging {
 
   val config = ConfigFactory.load()
+  implicit val system: ActorSystem = ActorSystem(config.getString("akka.systemName"))
+  implicit val executionContext = system.dispatcher
+  implicit val materializer = ActorMaterializer()
 
   val serverAddress = config.getString("akka.http.address")
   val serverPort = config.getInt("akka.http.port")
 
+  val routes = path("hello") {
+    get {
+      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem(config.getString("akka.systemName"))
-    implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
 
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
+    scala.sys.addShutdownHook {
+      system.terminate()
+      Await.result(system.whenTerminated, 5 seconds)
+    }
+
+    val t = new Thread(new Runnable {
+      override def run(): Unit = {
+        Http().bindAndHandle(routes, serverAddress, serverPort)
       }
-
-    val server = Http().bindAndHandle(route, serverAddress, serverPort)
-
-    server
-      .flatMap(_.unbind())
-      .onComplete(_ => system.terminate())
+    })
   }
 }
